8ea82ac80e3d292a9549f05b9c8f69e8135b2728
pbushel <pbushel@yahoo.com>
2026-05-27 22:26:37 -0400

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -62,9 +62,14 @@ exports.handler = async (event) => {
     });
 
     return {
-      statusCode: 200,
-      body: JSON.stringify({ url: session.url })
-    };
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
+    },
+    body: JSON.stringify({ url: session.url })
+  };
 
   } catch (error) {
     return {
